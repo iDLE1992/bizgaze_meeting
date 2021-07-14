14d7dd1b981b2b9abe7026806a73c338365194ad
denis2021-dev <79215287+denis2021-dev@users.noreply.github.com>
2021-07-15 02:12:45 +0900

    fix gitignore

--- videoconf/scripts/meeting.ts
+++ videoconf/scripts/meeting.ts
@@ -91,8 +91,8 @@ export class BizGazeMeeting {
     jitsiConnection: any;
 
 
-    //JitsiServerDomain = "idlests.com";
-    JitsiServerDomain = "unimail.in";
+    JitsiServerDomain = "idlests.com";
+    //JitsiServerDomain = "unimail.in";
 
     localTracks: JitsiTrack[] = [];
 
